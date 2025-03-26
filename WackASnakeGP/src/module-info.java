12c68f5347c268c8a5ab92301d014a956c440bfb
Jamison Studley <Studleyj@wit.edu>
2025-03-26 12:18:30 -0400

    Stars Screen and end screen

--- WackASnakeGP/src/module-info.java
+++ WackASnakeGP/src/module-info.java
@@ -1,6 +1,8 @@
 module WackASnakeGP {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
